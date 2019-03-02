b7c0ffbc0d4f677705dac49d194f52cb1f37e891
Bilal Azam <bilal.azam1@outlook.com>
2019-03-02 00:00:00 +0000

    Add cancellation support to useAsync

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -2,16 +2,21 @@ import { useState, useEffect } from 'react';
 
 export function useAsync(asyncFunction, dependencies = []) {
     const [isLoading, setLoading] = useState(true);
+    const controller = new AbortController();
 
     useEffect(() => {
         setLoading(true);
-        asyncFunction().then(response => {
+        asyncFunction(controller.signal).then(response => {
             setData(response);
             setLoading(false);
         }).catch(error => {
-            setError(error);
-            setLoading(true);
+            if (error.name !== 'AbortError') {
+                setError(error);
+            }
+            setLoading(false);
         });
+
+        return () => controller.abort();
     }, [...dependencies, asyncFunction]);
 
     return { isLoading, data, error };
