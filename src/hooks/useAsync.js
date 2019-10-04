4101269537c963e5eda433c301d44c61bf42940b
Bilal Azam <bilal.azam1@outlook.com>
2019-10-05 00:00:00 +0100

    Implement basic Suspense support in useAsync

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,1 +1,21 @@
-// Quick fixes and patch release updates in response to initial feedback.
+import { useState, useEffect, Suspense } from 'react';
+
+export function useAsync(asyncFunction, dependencies = []) {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        asyncFunction().then(response => {
+            setData(response);
+            setLoading(false);
+        }).catch(error => {
+            setError(error);
+            setLoading(true);
+        });
+    }, [...dependencies, asyncFunction]);
+
+    if (isLoading) throw new Suspense();
+    return { data, error };
+}
