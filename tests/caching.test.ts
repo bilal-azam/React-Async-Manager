590ceb595f7f29b6a49902a31d24bfe494305e99
Bilal Azam <bilal.azam1@outlook.com>
2020-09-10 00:00:00 +0100

    Test caching mechanism with various scenarios

---
+++ tests/caching.test.ts
@@ -0,0 +1,1 @@
+import { useAsync } from '../hooks/useAsync'; describe('Caching tests', () => { test('should return cached data without refetching', () => { // Test setup and assertions for caching behavior }); });
