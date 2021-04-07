09c6271215c5e0e377abc5a98d5b589dd81d888a
Bilal Azam <bilal.azam1@outlook.com>
2021-04-08 00:00:00 +0100

    Setup base error handling framework

---
+++ src/framework/errorHandling.ts
@@ -0,0 +1,7 @@
+// Setup the initial error handling framework
+export class ErrorHandler {
+    static handleException(error) {
+        console.error('Caught an error:', error);
+        // More sophisticated error handling logic here
+    }
+}
