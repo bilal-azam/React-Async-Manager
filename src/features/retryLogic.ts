623f791b0f47319c03a7ea11e5461a72c8930046
Bilal Azam <bilal.azam1@outlook.com>
2021-05-10 00:00:00 +0100

    Add error handling for retry logic

--- src/features/retryLogic.ts
+++ src/features/retryLogic.ts
@@ -1,4 +1,8 @@
-// Setup initial structure for retry logic
-export function setupRetryLogic() {
-    console.log('Setting up initial retry logic...');
+// Implement error handling within retry logic
+export function handleError(error) {
+    console.error('Handling error in retry logic', error);
+    return retryOperation();
+}
+function retryOperation() {
+    // Logic to retry the failed operation
 }
