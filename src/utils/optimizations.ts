6c08978a853cf6587ffd5ee0d76429ff88c6ff37
Bilal Azam <bilal.azam1@outlook.com>
2021-07-20 00:00:00 +0100

    Optimize retry intervals

---
+++ src/utils/optimizations.ts
@@ -0,0 +1,5 @@
+// Optimize the intervals between retries
+export function setRetryIntervals(interval) {
+    console.log('Setting retry intervals to', interval);
+    // Additional logic to adjust intervals
+}
