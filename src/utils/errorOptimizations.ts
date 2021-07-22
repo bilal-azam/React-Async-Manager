e900f3ac6c7fcf1271fa02d8f79808d8b71f057c
Bilal Azam <bilal.azam1@outlook.com>
2021-07-23 00:00:00 +0100

    Optimize error reporting

---
+++ src/utils/errorOptimizations.ts
@@ -0,0 +1,5 @@
+// Enhance error reporting to be more informative
+export function enhanceReporting(error) {
+    // Add logic to extract and format detailed error information
+    return `Error: ${error.message}`;
+}
