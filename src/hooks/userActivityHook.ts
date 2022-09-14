d514548d7d6cf73f5fe7ce70c08c2b0fda6710ee
Bilal Azam <bilal.azam1@outlook.com>
2022-09-15 00:00:00 +0100

    Implement user activity tracking event hook

---
+++ src/hooks/userActivityHook.ts
@@ -0,0 +1,5 @@
+// Implement event hook for tracking user activity
+export function useUserActivityHook() {
+    // Logic to track and log user activity
+    console.log('User Activity Hook set up');
+}
