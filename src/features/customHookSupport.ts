2a90e696b02dc2353e2c2d8217b44dd1413ec4f2
Bilal Azam <bilal.azam1@outlook.com>
2022-11-25 00:00:00 +0000

    Add support for custom hook definitions by users

---
+++ src/features/customHookSupport.ts
@@ -0,0 +1,5 @@
+// Enable users to define their own custom hooks
+export function allowCustomHooks(definition) {
+    // Implement functionality to register user-defined hooks
+    console.log('Custom Hook support added');
+}
