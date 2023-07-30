6cfa171d450cd4d6ff3d7fae1c561ae010ca8913
Bilal Azam <bilal.azam1@outlook.com>
2023-07-31 00:00:00 +0100

    Add unit tests for newly developed event hooks

---
+++ tests/unit/eventHooksTests.ts
@@ -0,0 +1,7 @@
+// Unit tests for July's newly developed event hooks
+describe('Event Hooks Unit Tests', () => {
+    it('should correctly initialize session management hook', () => {
+        // Test initialization logic
+        expect(useSessionManagement).toBeDefined();
+    });
+});
