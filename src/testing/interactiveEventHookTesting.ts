58e24a92ab10255878619388f1daa5ab524591d4
Bilal Azam <bilal.azam1@outlook.com>
2023-09-15 00:00:00 +0100

    Implement an interactive event hook testing interface

---
+++ src/testing/interactiveEventHookTesting.ts
@@ -0,0 +1,5 @@
+// Interactive interface for testing event hooks
+export function interactiveEventHookTesting() {
+    // Interface setup for users to test their event hooks in a simulated environment
+    console.log('Interactive event hook testing interface launched.');
+}
