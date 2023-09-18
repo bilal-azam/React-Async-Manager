8d8d4c0f208414a0435ff32b3ac23d8018a431ac
Bilal Azam <bilal.azam1@outlook.com>
2023-09-19 00:00:00 +0100

    Add new event types and handlers

---
+++ src/events/newEventTypes.ts
@@ -0,0 +1,8 @@
+// Add new event types and corresponding handlers
+export const newEventTypes = {
+    TYPE_A: 'Type A',
+    TYPE_B: 'Type B'
+};
+export function handleNewEventType(event) {
+    console.log('Handling new event type:', event);
+}
